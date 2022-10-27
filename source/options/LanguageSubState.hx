bc691838ed1c5307dbdbf64be0a6bc3243a0f8c4
Erlicer <95727981+Erlicer@users.noreply.github.com>
2022-10-27 05:52:59 -0500

    Update LanguageSubState.hx

--- source/options/LanguageSubState.hx
+++ source/options/LanguageSubState.hx
@@ -1,1 +1,57 @@
+package options;
 
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+import openfl.Lib;
+
+using StringTools;
+
+class LanguageSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Language';
+		rpcTitle = 'Language Settings Menu'; //for Discord Rich Presence
+
+		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
+		var option:Option = new Option('English', //Name
+			'If checked, enable english language.', //Description
+			'english', //Save data variable name
+			'bool', //Variable type
+			false); //Default value
+		addOption(option);
+
+		//I'd suggest using "Low Quality" as an example for making your own option since it is the simplest here
+		var option:Option = new Option('Portugues', //Name
+			'If checked, enable portuguese language.', //Description
+			'portuguese', //Save data variable name
+			'bool', //Variable type
+			false); //Default value
+		addOption(option);
+
+		super();
+	}
+}
