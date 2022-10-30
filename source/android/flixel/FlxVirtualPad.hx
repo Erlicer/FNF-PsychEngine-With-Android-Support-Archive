a777461fe9f4003d1f00a18de5ed0b5932ae1c9c
Erlicer <95727981+Erlicer@users.noreply.github.com>
2022-10-29 21:07:05 -0500

    Update FlxVirtualPad.hx

--- source/android/flixel/FlxVirtualPad.hx
+++ source/android/flixel/FlxVirtualPad.hx
@@ -1,45 +1,21 @@
 package android.flixel;
 
+import android.flixel.FlxButton;
 import flixel.FlxG;
-import flixel.graphics.FlxGraphic;
-import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxTileFrames;
 import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
-import android.flixel.FlxButton;
-import android.utils.Assets;
-
-enum FlxDPadMode
-{
-	UP_DOWN;
-	LEFT_RIGHT;
-	UP_LEFT_RIGHT;
-	LEFT_FULL;
-	RIGHT_FULL;
-	BOTH_FULL;
-	NONE;
-}
-
-enum FlxActionMode
-{
-	A;
-	B;
-	A_B;
-	A_B_C;
-	A_B_E;
-	A_B_X_Y;
-	A_B_C_X_Y;
-	A_B_C_X_Y_Z;
-	A_B_C_D_V_X_Y_Z;
-	NONE;
-}
+import flixel.util.FlxDestroyUtil;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.FlxGraphic;
+import openfl.utils.Assets;
 
 /**
  * A gamepad.
  * It's easy to customize the layout.
  *
- * @author Ka Wing Chin
- * @author Mihai Alexandru (M.A. Jigsaw)
+ * @original author Ka Wing Chin
+ * @modification's author: Saw (M.A. Jigsaw)
  */
 class FlxVirtualPad extends FlxSpriteGroup
 {
@@ -72,6 +48,8 @@ class FlxVirtualPad extends FlxSpriteGroup
 	public function new(DPad:FlxDPadMode, Action:FlxActionMode)
 	{
 		super();
+
+		scrollFactor.set();
 
 		switch (DPad)
 		{
@@ -153,8 +131,6 @@ class FlxVirtualPad extends FlxSpriteGroup
 				add(buttonA = createButton(FlxG.width - 132, FlxG.height - 135, 132, 127, 'a', 0xFF0000));
 			case NONE: // do nothing
 		}
-
-		scrollFactor.set();
 	}
 
 	/**
@@ -203,4 +179,29 @@ class FlxVirtualPad extends FlxSpriteGroup
 		#end
 		return button;
 	}
+}
+
+enum FlxDPadMode
+{
+	UP_DOWN;
+	LEFT_RIGHT;
+	UP_LEFT_RIGHT;
+	LEFT_FULL;
+	RIGHT_FULL;
+	BOTH_FULL;
+	NONE;
+}
+
+enum FlxActionMode
+{
+	A;
+	B;
+	A_B;
+	A_B_C;
+	A_B_E;
+	A_B_X_Y;
+	A_B_C_X_Y;
+	A_B_C_X_Y_Z;
+	A_B_C_D_V_X_Y_Z;
+	NONE;
 }
